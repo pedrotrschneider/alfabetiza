505282d74b1242333a610dcc1b8e39fd04a7ab92
Pedro Schneider <pedrotrschneider@gmail.com>
2021-11-02 00:12:53 -0300

    ✨ Update main sketch

--- src/sketch.js
+++ src/sketch.js
@@ -1,10 +1,12 @@
 let test, test2;
+let monke;
 
 function preload()
 {
     AssetHandler.loadTexture("monke", "/assets/textures/monke.png");
     AssetHandler.loadFont("Lato", "/assets/fonts/Lato-Regular.ttf");
     AssetHandler.loadAudio("bonk", "/assets/audio/thonk.wav");
+    gimoi = loadImage("/assets/textures/gimoi.png");
 }
 
 function setup()
@@ -14,7 +16,12 @@ function setup()
     GameHandler.init();
     textFont(AssetHandler.getP5FontByName("Lato"));
 
-    test2 = new Input("myInput", "", "text");
+    monke = AssetHandler.getP5ImageByName("monke");
+    let sf = new SpriteFrames();
+    sf.addAnimation("monke", monke, 4, 4, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+    test2 = new AnimatedSprite2D("myAnimSprite", null, sf);
+    test2.play();
+    test2.setCurrentFPS(1);
     GameHandler.addRootObject(test2);
 }
 
