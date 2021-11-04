797f403f4f0fe8f4b3bad9f7660d23d8b1e2bda4
Pedro Schneider <pedrotrschneider@gmail.com>
2021-11-03 22:36:17 -0300

    ✨ Add enumerators for property types, transition types and easing types

--- pandora/singletons/Enums.js
+++ pandora/singletons/Enums.js
@@ -19,17 +19,43 @@
  * along with Pandora.  If not, see <https://www.gnu.org/licenses/>.
  *************************************************************************/
 
+const PROPERTY_TYPE = {
+    NUMBER: 1,
+    VECTOR2: 2,
+    COLOR: 3,
+};
+
+const TRANS_TYPE = {
+    LINEAR: 1,
+    QUAD: 2,
+    CUBIC: 3,
+    QUART: 4,
+    QUINT: 5,
+    SINE: 6,
+    EXPONENTIAL: 7,
+    CIRCULAR: 8,
+    ELASTIC: 9,
+    BACK: 10,
+    BOUNCE: 11,
+};
+
+const EASE_TYPE = {
+    IN: 1,
+    OUT: 2,
+    IN_OUT: 3,
+};
+
 const SHAPES = {
     RECT: 1,
     ELLIPSE: 2,
-}
+};
 
 const RENDER_MODES = {
     P2D: 1,
     WEBGL: 2,
-}
+};
 
 const DEFAULT_STYLE = {
     "font-family": "Lato",
-    "font-size": "12px"
-}
+    "font-size": "12px",
+};
