a0cd4a6ff7e10067aaad9e168081811a613752cf
Pedro Tonini Rosenberg Schneider <pedrotrschneider@gmail.com>
2022-12-16 16:28:18 -0300

    0.1.22.12.16.0

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1669644995|3031843908";
+const CACHE_VERSION = "1671216917|18738973";
 const CACHE_PREFIX = "Alfabetiza-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
